f05f4a54036f7ec5fb987cd245009c69d294ae5e
gitihm <ismael897@outlook.com>
2019-06-24 09:39:42 -0700

    add ant desgin

---
+++ src/components/Page/Page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <Card title="Card title" bordered={false} style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+    </div>
+  );
+}
+
+export default App;
